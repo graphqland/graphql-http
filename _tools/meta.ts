06b819d15522941a60468a194f3dfeef99c1e9fd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-11 14:08:17 +0900

    chore(_tools): add build scripts

---
+++ _tools/meta.ts
@@ -0,0 +1,38 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.28.0/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {
+    deno: false,
+  },
+  compilerOptions: {
+    lib: ["dom", "es2022"],
+  },
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "gql-http",
+    version,
+    description:
+      "GraphQL on HTTP middleware with built-in validations and GraphQL playground",
+    keywords: [
+      "graphql",
+      "http",
+      "middleware",
+      "graphql-playground",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/TomokiMiyauci/graphql-http",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/TomokiMiyauci/graphql-http.git",
+    },
+    bugs: {
+      url: "https://github.com/TomokiMiyauci/graphql-http/issues",
+    },
+    sideEffects: false,
+    type: "module",
+  },
+  packageManager: "pnpm",
+});
