ade7c8148528af381f1fb9455179d11b449013ee
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-21 15:30:56 +0900

    chore: add types for GraphQL args

--- types.ts
+++ types.ts
@@ -1,5 +1,14 @@
+// deno-lint-ignore-file no-explicit-any
 import { APPLICATION_GRAPHQL_JSON, APPLICATION_JSON } from "./constants.ts";
-import { GraphQLError, json, PickBy } from "./deps.ts";
+import {
+  GraphQLError,
+  GraphQLFieldResolver,
+  GraphQLSchema,
+  GraphQLTypeResolver,
+  json,
+  PickBy,
+  Source,
+} from "./deps.ts";
 export type ApplicationJson = typeof APPLICATION_JSON;
 export type ApplicationGraphQLJson = typeof APPLICATION_GRAPHQL_JSON;
 
@@ -28,4 +37,34 @@ export type GraphQLParameters = {
 
   /** Reserved for implementors to extend the protocol however they see fit. */
   extensions: Record<string, json> | null;
+};
+
+export type GraphQLRequiredArgs = {
+  /** The GraphQL type system to use when validating and executing a query. */
+  schema: GraphQLSchema;
+
+  /** A GraphQL language formatted string representing the requested operation. */
+  source: string | Source;
+};
+
+export type GraphQLOptionalArgs = {
+  /** The value provided as the first argument to resolver functions on the top level type (e.g. the query object type). */
+  rootValue: unknown;
+
+  /** The context value is provided as an argument to resolver functions after field arguments. It is used to pass shared information useful at any point during executing this query, for example the currently logged in user and connections to databases or other services. */
+  contextValue: unknown;
+
+  /** A mapping of variable name to runtime value to use for all variables defined in the requestString. */
+  variableValues: {
+    readonly [variable: string]: unknown;
+  } | null;
+
+  /** The name of the operation to use if requestString contains multiple possible operations. Can be omitted if requestString contains only one operation. */
+  operationName: string | null;
+
+  /** A resolver function to use when one is not provided by the schema. If not provided, the default field resolver is used (which looks for a value or method on the source value with the field's name). */
+  fieldResolver: GraphQLFieldResolver<any, any> | null;
+
+  /** A type resolver function to use when none is provided by the schema. If not provided, the default type resolver is used (which looks for a `__typename` field or alternatively calls the `isTypeOf` method). */
+  typeResolver: GraphQLTypeResolver<any, any> | null;
 };
