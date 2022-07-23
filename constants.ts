86cf256272a0eb20c085f0959e89203da46225fe
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-23 21:29:16 +0900

    docs: add comment

--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
 export const APPLICATION_GRAPHQL_JSON = "application/graphql+json";
 export const APPLICATION_JSON = "application/json";
 
+/** Available charset. */
 export const CHARSET = "charset=UTF-8";
 
 export const MIME_TYPE_APPLICATION_GRAPHQL_JSON =
